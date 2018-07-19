e3629756b620ca6f4807d5b195cb33e22083d23d
Leron Julian <Leronjulian@hotmail.com>
2018-07-19 14:06:25 -0400

    updated miniLinks

--- src/components/SM_Links/Mini_Links.js
+++ src/components/SM_Links/Mini_Links.js
@@ -5,16 +5,10 @@ import linkedIn from "./ln.png"
 
 class Mini_Links extends React.Component{
     render() {
-        const styling = {
-            "padding-right": "20px",
-            "padding-top": "10px",
-            float: "left"
-        }
-
         return(
             <div>
-                <a href="https://github.com/leronjulian" target="_blank" rel="noopener noreferrer"> <img src={github} style={styling} alt="mini_picture" /></a>
-                <a href="https://www.linkedin.com/in/leronjulian/" target="_blank" rel="noopener noreferrer"> <img src={linkedIn}  style = {styling} alt="LinkedIn Logo"/> </a>   
+                <a href="https://github.com/leronjulian" target="_blank" rel="noopener noreferrer"> <img src={github} class="miniLinks" alt="mini_picture" /></a>
+                <a href="https://www.linkedin.com/in/leronjulian/" target="_blank" rel="noopener noreferrer"> <img src={linkedIn}  class="miniLinks" alt="LinkedIn Logo"/> </a>   
             </div>
         )
     }
