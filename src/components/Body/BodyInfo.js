5b7a09b113da47eab752d2f2a0ca593aab25d924
Leron Julian <Leronjulian@hotmail.com>
2018-07-20 11:04:52 -0400

    updated links

--- src/components/Body/BodyInfo.js
+++ src/components/Body/BodyInfo.js
@@ -7,7 +7,7 @@ class BodyInfo extends React.Component{
             <div>
                 <p class="BodyInfo"> 
                     I am a senior Computer Science Major at Morehouse College. I have published research under the supervision of Dr.
-                    Kinnis Gosha of the <a href="https://google.com" target="_blank" class="para" rel="noopener noreferrer">Culturally Relevant Computing Lab</a> at Morehouse College. This summer I am interning with NBCUniversal
+                    Kinnis Gosha of the <a href="http://diversitycomplete.com/crcl2/" target="_blank" class="para" rel="noopener noreferrer">Culturally Relevant Computing Lab</a> at Morehouse College. This summer I am interning with NBCUniversal
                     at the CNBC Headquarters in Englewood Cliffs, NJ working with Node.js, React.js, GraphQL, MongoDB, Centos, and Docker!
                     <br/><br/>
                     After Morehouse, I will go on to pursue a Ph.D. in Computer Science focusing on Machine Learning and Computer Vision. I
